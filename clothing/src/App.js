ea92689ed3f2c43a613e84fb68312916df66c183
Ruben Marin <staticparsley92@gmail.com>
2019-08-13 17:37:05 -0400

    added basic routing and previews

--- clothing/src/App.js
+++ clothing/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
-import HomePage from './hompage';
+
+import HomePage from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop';
 
 function App() {
   return (
-    <HomePage />
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
+    </div>
   );
 }
 
