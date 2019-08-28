ae2d827e3f0197b1f05f5d43492de22580bc292d
Ruben Marin <staticparsley92@gmail.com>
2019-08-28 17:52:49 -0400

    added header and signIn

--- clothing/src/App.js
+++ clothing/src/App.js
@@ -5,13 +5,17 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
