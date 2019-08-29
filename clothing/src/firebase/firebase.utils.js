55379a4dbf8bc14b9cd828408233d057441edca3
Ruben Marin <staticparsley92@gmail.com>
2019-08-28 20:55:11 -0400

    fixed firebase issue

--- clothing/src/firebase/firebase.utils.js
+++ clothing/src/firebase/firebase.utils.js
@@ -13,36 +13,38 @@ const config = {
     appId: "1:435035587659:web:1397b628b1fb026a"
 };
 
+firebase.initializeApp(config);
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-    if(!userAuth) return;
+    if (!userAuth) return;
 
     const userRef = firestore.doc(`users/${userAuth.uid}`);
-    const snapShot = await userRef.get();
-    if(!snapShot.exists) {
-        const {displayName, email} = userAuth;
-        const createdAt = new Date();
 
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
         try {
             await userRef.set({
                 displayName,
                 email,
                 createdAt,
                 ...additionalData
-            })
-        }catch(error) {
+            });
+        } catch (error) {
             console.log('error creating user', error.message);
-
         }
     }
-}
 
-firebase.initializeApp(config);
+    return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({prompt: 'select_account'});
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
